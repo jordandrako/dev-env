9b9945beb16194512f38ac2a1a87f769a89af08f
Jordan Janzen <jordancjanzen@gmail.com>
2018-06-13 11:16:40 -0700

    Add cygwin bash as choice

--- cmder/shellchooser.bat
+++ cmder/shellchooser.bat
@@ -4,24 +4,26 @@
 CLS
 ECHO Choose a shell:
 ECHO [1] WSL
-ECHO [2] ZSH
-ECHO [3] CMDER cmd
-ECHO [4] CMDER PowerShell
-ECHO [5] cmd
-ECHO [6] Powershell
+ECHO [2] Cygwin ZSH
+ECHO [3] Cygwin Bash
+ECHO [4] CMDER cmd
+ECHO [5] CMDER PowerShell
+ECHO [6] cmd
+ECHO [7] Powershell
 ECHO.
 ECHO [8] restart hyper elevated
 ECHO [9] exit
 ECHO.
 
-CHOICE /N /C:12345689 /D 1 /T 10 /M "> "
+CHOICE /N /C:123456789 /D 1 /T 10 /M "> "
 CLS
 IF ERRORLEVEL ==9 GOTO end
 IF ERRORLEVEL ==8 powershell -Command "Start-Process hyper -Verb RunAs"
-IF ERRORLEVEL ==6 powershell
-IF ERRORLEVEL ==5 cmd
-IF ERRORLEVEL ==4 GOTO CMDERPOWERSHELL
-IF ERRORLEVEL ==3 GOTO CMDERCMD
+IF ERRORLEVEL ==7 powershell
+IF ERRORLEVEL ==6 cmd
+IF ERRORLEVEL ==5 GOTO CMDERPOWERSHELL
+IF ERRORLEVEL ==4 GOTO CMDERCMD
+IF ERRORLEVEL ==3 GOTO CYGBASH
 IF ERRORLEVEL ==2 GOTO CYGZSH
 IF ERRORLEVEL ==1 GOTO WSL
 
@@ -34,6 +36,10 @@ SET CMDER_ROOT=C:\cmder
 IF EXIST %CMDER_ROOT% (
   cmd /k "%CMDER_ROOT%\vendor\init.bat"
 )
+goto FALLBACK
+
+:CYGBASH
+C:\cygwin64\bin\bash.exe
 goto FALLBACK
 
 :CYGZSH
