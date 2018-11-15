39515676b6bba63f7f54cf2ed13549ea23f8eebd
Jordan <jordancjanzen@gmail.com>
2018-11-15 10:54:30 -0800

    wsl xserver and cmd aliases, more specific /mnt check

--- dotfiles/.wsl.zsh
+++ dotfiles/.wsl.zsh
@@ -3,10 +3,17 @@
 ## WSL configs
 unsetopt BG_NICE
 export MACHINE="WSL"
-[[ -d /c ]] && export CODE_DIR=/c/code || export CODE_DIR=/mnt/c/code
+[[ -d /c/code ]] && export CODE_DIR=/c/code || export CODE_DIR=/mnt/c/code
 
 alias coder="cd $CODE_DIR"
 alias nsfi="cd $CODE_DIR && cd ui-fabric-website-internal/apps/fabric-website-internal && npm start"
+
+if [[ -x "$(command -v cmd.exe)" ]]; then
+  alias cmd="cmd.exe"
+  alias x410="cmd.exe /c 'start x410.exe'"
+  alias xkill="cmd.exe /c 'taskkill /IM x410.exe'"
+  alias xdesktop="cmd.exe /c 'taskkill /IM x410.exe' && cmd.exe /c 'start x410 /desktop'"
+fi
 
 if [[ -x "$(command -v docker.exe)" && ! -x "$(command -v docker)" ]]; then
   alias docker="docker.exe"
