81a441ebf71e998e0fc28310bb68a48ce775e4c8
Jordan Janzen <jordancjanzen@gmail.com>
2018-11-27 16:07:35 -0800

    colorls

--- dotfiles/.aliases.sh
+++ dotfiles/.aliases.sh
@@ -5,32 +5,41 @@ setopt auto_pushd
 setopt pushd_ignore_dups
 setopt pushdminus
 
-alias -g ...='../..'
-alias -g ....='../../..'
-alias -g .....='../../../..'
-alias -g ......='../../../../..'
+alias -g ...="../.."
+alias -g ....="../../.."
+alias -g .....="../../../.."
+alias -g ......="../../../../.."
 
-alias -- -='cd -'
-alias 1='cd -'
-alias 2='cd -2'
-alias 3='cd -3'
-alias 4='cd -4'
-alias 5='cd -5'
-alias 6='cd -6'
-alias 7='cd -7'
-alias 8='cd -8'
-alias 9='cd -9'
+alias -- -="cd -"
+alias 1="cd -"
+alias 2="cd -2"
+alias 3="cd -3"
+alias 4="cd -4"
+alias 5="cd -5"
+alias 6="cd -6"
+alias 7="cd -7"
+alias 8="cd -8"
+alias 9="cd -9"
 
-alias md='mkdir -p'
+alias md="mkdir -p"
 alias rd=rmdir
-alias d='dirs -v | head -10'
+alias d="dirs -v | head -10"
 
 # List directory contents
-alias ls='ls --color=always'
-alias lsa='ls -lah'
-alias l='ls -lah'
-alias ll='ls -lh'
-alias la='ls -lAh'
+if [[ -x "$(command -v colorls)" ]]; then
+  alias lc="colorls -lA --sd"
+  alias ls="colorls"
+  alias lsa="colorls -la"
+  alias l="colorls -la --sd"
+  alias ll="colorls -l --sd"
+  alias la="colorls -lA --sd"
+else
+  alias ls="ls --color=always"
+  alias lsa="ls -lah"
+  alias l="ls -lah"
+  alias ll="ls -lh"
+  alias la="ls -lAh"
+fi
 
 # Git
 if [[ -x "$(command -v git)" ]]; then
@@ -97,7 +106,6 @@ if [[ -x "$(command -v git)" ]]; then
 else
   echo "Install git ya git!"
 fi
-
 
 # NPM
 if [[ -x "$(command -v npm)" ]]; then
