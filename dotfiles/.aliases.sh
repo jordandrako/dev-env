487abd249b12ccc9f3f2196498e25852bbcabc4d
Jordan Janzen <jordancjanzen@gmail.com>
2019-01-24 15:00:51 -0800

    add git fetch -p to prune local

--- dotfiles/.aliases.sh
+++ dotfiles/.aliases.sh
@@ -117,6 +117,7 @@ if [[ -x `command -v git` ]]; then
       case $pruneYn in
         [yY]* )
           git branch -r | awk '{print $1}' | egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk '{print $1}' | xargs git branch -D;
+          git fetch -p
           break;;
         * )
           info "Branches not removed.";
