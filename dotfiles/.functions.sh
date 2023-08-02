b6273dc6ff4056edb285f542098fa514527646eb
Jordan Janzen <jordan@morels.me>
2023-08-02 13:02:06 -0700

    Mac/old bash compat funcs

--- dotfiles/.functions.sh
+++ dotfiles/.functions.sh
@@ -2,17 +2,17 @@
 
 # Green background echo
 green() {
-  echo -e "\n\e[1;32m$1\e[0m\n"
+  echo -e "\n\x1B[1;32m$1\x1B[0m\n"
 }
 
 # Yellow background echo
 info() {
-  echo -e "\n\e[1;33m$1\e[0m\n"
+  echo -e "\n\x1B[1;33m$1\x1B[0m\n"
 }
 
 # Error echo
 error() {
-  echo -e "\n\e[1;5;31m$1\e[0m\n" 1>&2 && echo -ne '\007'
+  echo -e "\n\x1B[1;5;31m$1\x1B[0m\n" 1>&2 && echo -ne '\007'
 }
 
 # Perform task successfully or print failed
@@ -28,7 +28,7 @@ ask() {
   else
     question=$1
     input=${2:="y/n"}
-    echo -e "\n\e[1;34m$question\e[0m" && echo "[$input] > "
+    echo -e "\n\x1B[1;34m$question\x1B[0m" && echo "[$input] > "
     [[ $3 ]] && read $3
   fi
 }
