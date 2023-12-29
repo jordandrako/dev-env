a785c2770ec18955c8389369ead6ca16d78d6863
Jordan Janzen <jordan@morels.me>
2023-12-29 02:36:01 -0800

    fix wget

--- install_unraid.sh
+++ install_unraid.sh
@@ -3,23 +3,21 @@
 dl_path="https://raw.githubusercontent.com/jordandrako/dev-env/master/dotfiles"
 
 # Global configuration
-wget "${dl_path}/.zshrc" ~/
+wget "${dl_path}/.zshrc" ~/.zshrc
 
-wget "${dl_path}/.nanorc" ~/
+wget "${dl_path}/.nanorc" ~/.nanorc
 
-wget "${dl_path}/.tmux.conf" ~/
+wget "${dl_path}/.tmux.conf" ~/.tmux.conf
 
-wget "${dl_path}/.p10k.zsh" ~/
+wget "${dl_path}/.p10k.zsh" ~/.p10k.zsh
 
-wget "${dl_path}/.key-bindings.zsh" ~/.config/
+wget "${dl_path}/.key-bindings.zsh" ~/.config/.key-bindings.zsh
 
-wget "${dl_path}/.functions.zsh" ~/.config/
+wget "${dl_path}/.functions.zsh" ~/.config/.functions.zsh
 
-wget "${dl_path}/.aliases.zsh" ~/.config/
+wget "${dl_path}/.aliases.zsh" ~/.config/.aliases.zsh
 
-wget "${dl_path}/.functions.zsh" ~/.config/
-
-wget "${dl_path}/.linux.zsh" ~/.config/
+wget "${dl_path}/.linux.zsh" ~/.config/.linux.zsh
 
 # fzf plugin
 if [[ ! -f ~/.fzf.zsh ]]; then
